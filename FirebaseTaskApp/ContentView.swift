1a031039b1541d65a753e985acd0203ca3f1760e
sun-23 <twinsamson@gmail.com>
2019-11-20 18:19:06 +0700

    Initial Commit

---
+++ FirebaseTaskApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseTaskApp
+//
+//  Created by sun on 20/11/2562 BE.
+//  Copyright © 2562 sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
